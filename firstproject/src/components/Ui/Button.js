d79f31457bfd96449c2826f4a2d87310f96122df
IppEvg <ippevg@mail.ru>
2023-01-03 12:58:26 +0300

    Homework10

--- firstproject/src/components/Ui/Button.js
+++ firstproject/src/components/Ui/Button.js
@@ -3,7 +3,7 @@ import styles from '../func/message.module.scss'
 export function Button (props){
     return(
         <>
-        <button className={styles.button} {...props} >{props.children}</button>
+        <button className={styles.button}  {...props} onClick={props.click} >{props.children}</button>
         </>
     )
 }
